2bf361c1e950da21d0ced845966c0acb1cab79eb
Simon Sinding <sinding2000@gmail.com>
2017-05-06 15:59:56 +0200

    backup

---
+++ Projects/VOP1/src/Main.java
@@ -0,0 +1,18 @@
+/**
+ * Created by thesinding on 3/5/17.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
